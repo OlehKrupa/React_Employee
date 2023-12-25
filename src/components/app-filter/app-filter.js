0b1cc46067eb173b7f3c28341ca3ed620e4766e7
Oleh <krupao980@gmail.com>
2023-12-25 15:53:02 +0200

    Деструктуризация

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,6 +1,6 @@
 import "./app-filter.css";
 
-const AppFilter = (props) => {
+const AppFilter = ({filter, onFilterSelect}) => {
     const buttonsData = [
         {name: 'all',label: 'Все сотрудники'},
         {name: 'rise',label: 'На повышение'},
@@ -8,7 +8,7 @@ const AppFilter = (props) => {
     ];
 
     const buttons = buttonsData.map(({name, label}) => {
-        const active = props.filter === name;
+        const active = filter === name;
 
         const clazz = active ? 'btn-light' : 'btn-outline-light'
 
@@ -16,7 +16,7 @@ const AppFilter = (props) => {
             <button type="button"
                     className={`btn ${clazz}`}
                     key={name}
-                    onClick={() => props.onFilterSelect(name)}>
+                    onClick={() => onFilterSelect(name)}>
                     {label}
             </button>
         )
